fb2a5cea38577fb750335de6dad16dd9c4e3833a
Mehmet Ege Donmez <mehmetege0811@gmail.com>
2026-08-03 12:03:56 +0300

    Ders 1: Proje Oluşturuldu

---
+++ src/a.kt
@@ -0,0 +1,18 @@
+/* GitHub oluşturma ezber
+
+1) Create repocity de
+
+2) gitignore'a gel...
+
+    a) *.iml yazarak (tüm iml dosyalarını kaydetmesini engelle)
+
+    b) .idea/ yazarak (idea dosyasını kaydetmesini engelle)
+
+3) github'a git...
+
+    a) + butonuna tıpla ve new repository oluştur
+
+    b) oluşturduğun repository'nin url'ini kopyala
+
+    c) kotlinde üst kısımdaki üç çizgi'ye bas oradan git'e gel ve gitten "Manage Remotes" butonuna tıklayıp linki at.
+*/
