d74024c4a82d619f2be2b3f7e4254b934663c730
Mehmet Ege Donmez <mehmetege0811@gmail.com>
2026-08-14 12:45:11 +0300

    Ders 5.5: GitHub kur, klonla, commitlerde ileri-geri gitme

--- src/githubkurma.kt
+++ src/githubkurma.kt
@@ -1,6 +1,7 @@
 /* GitHub oluşturma ezber
 
-1) Intellijde create repocity de commit menusunden
+1) Intellijde create repocity de ve git dosyasını kaydedeceği klasörü seç, kaydetmek istediklerini seç ve commit menusunden isim ver
+(ama push yapamazsın çünkü gitte reposity oluşturmadın)
 
 2) gitignore'a gel...
 
@@ -14,5 +15,23 @@
 
     b) oluşturduğun repository'nin url'ini kopyala
 
-    c) kotlinde üst kısımdaki üç çizgi'ye bas oradan git'e gel ve gitten "Manage Remotes" butonuna tıklayıp linki at.
+    c) intellijde üst kısımdaki üç çizgi'ye bas oradan git'e gel ve gitten "Manage Remotes" butonuna tıklayıp linki at.
 */
+
+/* Github clone oluşturma
+
+1) Klonlamak istedigin repositye ait linki kopyala
+
+2) Projelerin buldunuğu kısımda Clone Reposity kısmına gel ve linki koy
+
+3) Proje bozuk ve hatalı çıkacaktır (iml dosyasını kaydetmediysen özellikle src dosyasını Mark As Directory - Sources Root ile Mavi yap)
+
+ */
+
+/* Github geri-ileri gitme projelerde
+
+1) Git sekmesini aç konsolların buldunuğu kısımlardan
+
+2) İstediginiz commiti sağ tıkların ve Checkout Resivation veya Checkout (master) seç
+
+ */
